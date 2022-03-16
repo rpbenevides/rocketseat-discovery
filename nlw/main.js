ee9aa687612e7c7ae3b76317228f98dad1d4689f
Rafael P Benevides <99306529+rpbenevides@users.noreply.github.com>
2022-03-16 17:22:09 -0300

    v4

--- nlw/main.js
+++ nlw/main.js
@@ -14,3 +14,13 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) header.classList.add('scroll')
+  else {
+    header.classList.remove('scroll')
+  }
+})
