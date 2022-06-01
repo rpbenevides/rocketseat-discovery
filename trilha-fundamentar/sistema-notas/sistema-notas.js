d82249a4b0559e139cf06c9ff6968d3ce6f452d9
Rafael Benevides <rafabenevides@pop-os.localdomain>
2022-05-31 22:48:07 -0300

    Continuação da trilha fundamentar.

---
+++ trilha-fundamentar/sistema-notas/sistema-notas.js
@@ -0,0 +1,20 @@
+function notaFinal(nota) {
+  const transform = transformarNotas(nota)
+  console.log(`A nota final do aluno foi conceito ${transform}.`)
+}
+
+function transformarNotas(nota) {
+  if (nota >= 90) {
+    return (nota = 'A')
+  } else if (nota <= 89 && nota >= 80) {
+    return (nota = 'B')
+  } else if (nota <= 79 && nota >= 70) {
+    return (nota = 'C')
+  } else if (nota <= 69 && nota >= 60) {
+    return (nota = 'D')
+  } else {
+    return (nota = 'F')
+  }
+}
+
+notaFinal(99)
